abac61987914605e1c70b18c2ade242f5e1b469d
David <birdearly834@gmail.com>
2025-12-22 11:06:10 +0800

    The 34th problem "Valid Sudoku" has been solved.

---
+++ src/main/java/problems/p034_ValidSudoku/Solution.java
@@ -0,0 +1,34 @@
+package problems.p034_ValidSudoku;
+
+import java.util.HashSet;
+
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        //遍历整个矩阵：
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                char ch = board[i][j];
+                int boxIndex = i / 3 * 3 + j / 3;
+                if (rows[i].contains(ch) || cols[j].contains(ch) || boxes[boxIndex].contains(ch)) {
+                    return false;
+                }
+                rows[i].add(ch);
+                cols[j].add(ch);
+                boxes[boxIndex].add(ch);
+            }
+        }
+        return true;
+    }
+}
